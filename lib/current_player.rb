d01848bdb8505e16c783be98b3813d04d5df25f3
steven fox <steven.fox86@gmail.com>
2020-09-15 14:47:00 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@
 def turn_count(board)
   counter = 0
   board.each do |token|
-    if board[token] == "X" || board[token] == "O"
+    if board[index] == "X" || board[index] == "O"
   counter += 1
  end
 end
