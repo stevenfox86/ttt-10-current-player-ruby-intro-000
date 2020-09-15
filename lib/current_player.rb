1ac6ce9b854313649c389d87aef9a7f24d54f328
steven fox <steven.fox86@gmail.com>
2020-09-15 14:05:32 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -7,9 +7,9 @@ end
 end
 
 def current_player(board)
-  if turn_count(board) % 2 = 0
+  if turn_count(board) % 2 == 0
   X
-  else turn_count(board) & 2 = 1
+else turn_count(board) & 2 == 1
   O
 end
 end
